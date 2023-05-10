6b5bb6942d103881f934c1d404c5a686583bad39
watchm1 <m.ali.software.dev@gmail.com>
2023-05-10 06:10:17 +0300

    updateed routes

--- server/routes/orderRoutes.ts
+++ server/routes/orderRoutes.ts
@@ -7,11 +7,11 @@ const orderRouter: Router = express.Router();
 orderRouter.route('/new-order').post(auth, newOrder);
 orderRouter.route('/user/:user').get(auth, getUserOrders);
 orderRouter.route('/user/:user/:order').get(auth, getUserOrderDetails);
-orderRouter.route('/user/refund').delete(auth, refundOrder);
 //// admin routes
 orderRouter.route('/').get(auth, getAllOrders).delete(auth, hardResetOrders);
 orderRouter.route('/admin/details/:order').get(auth, adminViewOrderDetail);
 orderRouter.route('/admin/to-shipping/:order').put(auth, orderToShipping);
-orderRouter.route('/admin/cancel/:order').delete(auth, cancelOrderByID);
+orderRouter.route('/admin/refund/:order').delete(auth, refundOrder);
+orderRouter.route('/admin/cancel').delete(auth, cancelOrderByID);
 
 export default orderRouter;
--- server/routes/productRoutes.ts
+++ server/routes/productRoutes.ts
@@ -5,6 +5,9 @@ import { admin, auth } from '../middleware/auth';
 const router: Router = express.Router();
 
 router.route('/').get(getAllProducts).post(auth, admin, createProduct);
-router.route('/:id').delete(auth, admin, deleteProduct).put(auth, admin, updateProduct).get(getProductById);
+router.route('/:id').get(getProductById);
+// admin side
+router.route('/admin/products')
+router.route('/admin/products/:id').delete(auth, admin, deleteProduct).put(auth, admin, updateProduct)
 
 export default router;
